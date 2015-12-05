a139e480e11cec1318c6c6b569fe4c23a67801b1
Achint Sandhu <sandhu@7theta.com>
2015-12-05 15:08:06 -0500

    Upgrade test dependencies

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any others, from this software.
 
-(defproject com.7theta/utilis "0.4.4"
+(defproject com.7theta/utilis "0.4.5"
   :description "Library of common utilities used in 7theta projects"
   :url "https://github.com/7theta/utilis"
   :license {:name "Eclipse Public License"
@@ -17,10 +17,10 @@
                  [org.clojure/clojurescript "1.7.170"]]
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
                    :plugins [[lein-cljsbuild "1.1.1"]
-                             [lein-doo "0.1.6-rc.1"]]
-                   :dependencies [[reloaded.repl "0.2.0"]
+                             [lein-doo "0.1.6"]]
+                   :dependencies [[reloaded.repl "0.2.1"]
                                   [org.clojure/tools.namespace "0.2.11"]
-                                  [org.clojure/test.check "0.8.1"]
+                                  [org.clojure/test.check "0.9.0"]
                                   [com.gfredericks/test.chuck "0.2.3"]]
                    :source-paths ["dev"]}}
   :clean-targets ^{:protect false} ["out" "target"]
