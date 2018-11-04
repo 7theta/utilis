169f63572bacf719f2374791a78241ff4eecabb0
Achint Sandhu <sandhu@7theta.com>
2018-11-04 16:56:39 -0500

    Upgrade build and dependencies

--- project.clj
+++ project.clj
@@ -14,13 +14,18 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
+  :jvm-opts ~(let [version (System/getProperty "java.version")
+                   [major _ _] (clojure.string/split version #"\.")]
+               (if (>= (java.lang.Integer/parseInt major) 9)
+                 ["--add-modules" "java.xml.bind"]
+                 []))
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
                    :plugins [[lein-cljsbuild "1.1.7"]
                              [lein-doo "0.1.7"]]
-                   :dependencies [[org.clojure/clojurescript "1.9.946"]
+                   :dependencies [[org.clojure/clojurescript "1.10.439"]
                                   [org.clojure/tools.namespace "0.2.11"]
                                   [org.clojure/test.check "0.9.0"]
-                                  [com.gfredericks/test.chuck "0.2.8"]]
+                                  [com.gfredericks/test.chuck "0.2.9"]]
                    :source-paths ["dev"]}}
   :clean-targets ^{:protect false} ["out" "target"]
   :cljsbuild {:builds [{:id "test"
